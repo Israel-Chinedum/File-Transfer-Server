c59bf110fa5a46c0d2edc8a2d3d7df6dfe97e3c4
Israel Favour <fastarfavour@gmail.com>
2025-08-13 10:32:46 -0700

    added the handleConn service to the socket file

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,10 +1,17 @@
 import { testLogic } from "../services/socket_services/test_logic.js";
 import { handleChunk } from "../services/socket_services/handle_chunk.js";
+import { handleConn } from "../services/socket_services/handleConn.js";
+import { readyConn } from "../server.js";
+
+
 export const socket = (io) => {
   console.log("running");
   io.on("connection", (socket) => {
     console.log("New user connected!");
     socket.on('test', testLogic);
+
+    // USE SERVICES
     handleChunk(socket);
+    handleConn(socket, readyConn);
   });
 };
