48e56a9b916ec9305d65eb986f890b5168c769d9
Israel Favour <fastarfavour@gmail.com>
2025-08-14 10:39:55 -0700

    made changes

--- src/services/socket_services/handleConn.js
+++ src/services/socket_services/handleConn.js
@@ -1,18 +1,21 @@
 export const handleConn = ( socket, readyConn ) => {
     socket.on('waiting', () => {
         console.log('SOCKET ID', socket.id);
-        readyConn.set(socket.id, socket.id);
+        readyConn.set(`User ${readyConn.size+1}`, socket.id);
     });
 
     socket.on('searchConn', () => {
+        console.log('A search request was made')
         if(readyConn.size){
-            socket.emit('readyConn', readyConn);
+            const keys = Array.from(readyConn.keys())
+            socket.emit('readyConn', keys);
         } else{
             setTimeout(() => {
                 if(!readyConn.size){
                     socket.emit('error', 'No connection found!');
                 } else{
-                    socket.emit('readyConn', readyConn);
+                    const keys = Array.from(readyConn.keys())
+                    socket.emit('readyConn', keys);
                 }
             }, 20000);
         }
